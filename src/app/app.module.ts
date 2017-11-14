e8c3a51f3f705f98d0695fa6ac4c68e9af08e86c
sanchayt <sanchayt37@yahoo.com>
2017-11-13 22:49:27 -0500

    Navbar and router outlet created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { StopwatchComponent } from './stopwatch/stopwatch.component';
+import { Routes } from '@angular/router/src/config';
+
+const appRoutes: Routes =
+  [
+    {
+      path: 'stopwatch',
+      component: StopwatchComponent
+    },
+    {
+      path: 'servers',
+      component: ServersComponent
+    }
+  ];
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    StopwatchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
