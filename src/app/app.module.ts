b6a24196705cfacd1895102119ac83ebe086a416
sanchayt <sanchayt37@yahoo.com>
2017-11-17 23:55:01 -0500

    Added angular 4 material configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { MatTabsModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { StopwatchComponent } from './stopwatch/stopwatch.component';
 import { Routes } from '@angular/router/src/config';
+import { CategoriesComponent } from './categories/categories.component';
 
 const appRoutes: Routes =
   [
@@ -29,12 +33,16 @@ const appRoutes: Routes =
     AppComponent,
     ServerComponent,
     ServersComponent,
-    StopwatchComponent
+    StopwatchComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatToolbarModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
