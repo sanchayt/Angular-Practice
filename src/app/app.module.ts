48d1dccad37f9df352e407f95b310f74bb369910
sanchayt <sanchayt37@yahoo.com>
2018-08-13 21:52:37 -0400

    App component essential files and assets added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
-import { MatTabsModule, MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
+// import { MatTabsModule, MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-
-
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -16,6 +15,14 @@ import { Routes } from '@angular/router/src/config';
 import { CategoriesComponent } from './categories/categories.component';
 import { TodoComponent } from './todo/todo.component';
 import { CaseReversalComponent } from './case-reversal/case-reversal.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './services/hero.service';
+import { MessageComponent } from './message/message.component';
+import { MessageService } from './services/message.service';
+import { MapsItemComponent } from './maps-item/maps-item.component';
+import { OffersComponent } from './offers/offers.component';
+import { GroupsComponent } from './groups/groups.component';
 
 const appRoutes: Routes =
   [
@@ -34,6 +41,14 @@ const appRoutes: Routes =
     {
       path: 'case-reversal',
       component: CaseReversalComponent
+    },
+    {
+      path: 'heroes',
+      component: HeroesComponent
+    },
+    {
+      path: 'offers',
+      component: OffersComponent
     }
   ];
 
@@ -48,20 +63,27 @@ const appRoutes: Routes =
     StopwatchComponent,
     CategoriesComponent,
     TodoComponent,
-    CaseReversalComponent
+    CaseReversalComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessageComponent,
+    MapsItemComponent,
+    OffersComponent,
+    GroupsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
+    HttpClientModule,
+    // MatTabsModule,
+    // MatToolbarModule,
+    // MatButtonModule,
+    // MatIconModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
